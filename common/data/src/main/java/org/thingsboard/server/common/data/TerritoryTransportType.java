363c37c38aa64acb3f7386463a212b31a8c4e7ac
Vladislav Arsenev <forjavatests8@gmail.com>
2021-07-19 11:26:40 +0300

    WIP

---
+++ common/data/src/main/java/org/thingsboard/server/common/data/TerritoryTransportType.java
@@ -0,0 +1,8 @@
+package org.thingsboard.server.common.data;
+
+public enum TerritoryTransportType {
+    DEFAULT,
+    MQTT,
+    LWM2M,
+    COAP
+}
--- dao/src/main/java/org/thingsboard/server/dao/territory/TerritoryServiceImpl.java
+++ dao/src/main/java/org/thingsboard/server/dao/territory/TerritoryServiceImpl.java
@@ -1,17 +1,10 @@
 package org.thingsboard.server.dao.territory;
 
-import com.google.common.util.concurrent.ListenableFuture;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.thingsboard.server.common.data.Territory;
-import org.thingsboard.server.common.data.id.TenantId;
 import org.thingsboard.server.dao.entity.AbstractEntityService;
-import org.thingsboard.server.dao.tenant.TenantDao;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 @Slf4j
