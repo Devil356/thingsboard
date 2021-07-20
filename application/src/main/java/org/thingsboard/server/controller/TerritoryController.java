aa21b61895aa957f9d67439c6bd54408ad534077
Devil356 <devil208356@gmail.com>
2021-07-20 22:10:40 +0200

    Add licence

--- application/src/main/java/org/thingsboard/server/controller/TerritoryController.java
+++ application/src/main/java/org/thingsboard/server/controller/TerritoryController.java
@@ -1,11 +1,24 @@
+/**
+ * Copyright © 2016-2021 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.thingsboard.server.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.thingsboard.server.common.data.Territory;
 import org.thingsboard.server.common.data.exception.ThingsboardException;
-import org.thingsboard.server.dao.territory.TerritoryService;
 import org.thingsboard.server.queue.util.TbCoreComponent;
 
 @RestController
