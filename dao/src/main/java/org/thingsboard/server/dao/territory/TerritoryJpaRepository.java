b9683aed3a1fb06b8f19a59fa920720aa09a7728
Devil356 <devil208356@gmail.com>
2021-07-20 22:05:09 +0200

    Add annotation for repo

--- dao/src/main/java/org/thingsboard/server/dao/territory/TerritoryJpaRepository.java
+++ dao/src/main/java/org/thingsboard/server/dao/territory/TerritoryJpaRepository.java
@@ -17,11 +17,13 @@ package org.thingsboard.server.dao.territory;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.thingsboard.server.common.data.Territory;
 import org.thingsboard.server.common.data.id.TenantId;
 import org.thingsboard.server.dao.Dao;
 import org.thingsboard.server.dao.TenantEntityDao;
 
+@Repository
 public interface TerritoryJpaRepository extends JpaRepository<Territory, Long> {
 
     /**
