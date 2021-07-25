455adc6f4a4a777fc55fee8906298ecf5fd5d37a
Devil356 <devil208356@gmail.com>
2021-07-25 18:47:37 +0200

    Complete services for entities

---
+++ dao/src/main/java/org/thingsboard/server/dao/megadevice/MegaDeviceServiceImpl.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright © 2016-2021 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.server.dao.megadevice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.thingsboard.server.dao.building.BuildingJpaRepository;
+import org.thingsboard.server.dao.model.sql.MegaDeviceEntity;
+import org.thingsboard.server.dao.model.sql.RoomEntity;
+import org.thingsboard.server.dao.room.RoomJpaRepository;
+import org.thingsboard.server.dao.room.RoomService;
+
+@Service
+public class MegaDeviceServiceImpl implements MegaDeviceService {
+
+    @Autowired
+    private MegaDeviceJpaRepository megaDeviceJpaRepository;
+
+    @Autowired
+    private RoomJpaRepository roomJpaRepository;
+
+    @Override
+    public MegaDeviceEntity save(MegaDeviceEntity megaDeviceEntity, Integer roomId) {
+        megaDeviceEntity.setRoomEntity(roomJpaRepository.getOne(roomId));
+        return megaDeviceJpaRepository.save(megaDeviceEntity);
+    }
+}
